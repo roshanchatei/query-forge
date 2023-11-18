90d25187464079843114f671131bc9476f539aed
roshanchatei <rkc.mj5@gmail.com>
2023-11-18 12:31:07 +0530

    editor layout done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,18 +17,27 @@ const Navbar = () =>  {
                 }}
             >
                 <Box py={1.3} px={2} display={'flex'} alignItems={'center'} justifyContent={'space-between'} >
-                    <img src={Logo} alt={'Atlan logo'} height={'40px'} />
+                    <img src={Logo} alt={'Atlan logo'} height={'35px'} width={'auto'} />
                     <Box display={'flex'} alignItems={'center'}>
                         {
                             navbarLinks.map((each, index) => (
                                 <Link to={each.link} key={index} target={each?.target} style={{textDecoration: "none"}}>
-                                    <Box ml={2}>
+                                    <Box
+                                        ml={3} color={"#2026d2"}
+                                        fontSize={'16px'} fontWeight={500}
+                                        fontFamily={"Raleway,sans-serif"}
+                                        sx={{
+                                            "&:hover": {
+                                                color: "#6c72ff",
+                                            },
+                                        }}
+                                    >
                                         {each.title}
                                     </Box>
                                 </Link>
                             ))
                         }
-                        <Avatar src={'https://source.unsplash.com/featured/300x202'} sx={{background: '#2026d2', marginLeft: "30px"}} />
+                        <Avatar src={'https://source.unsplash.com/featured/300x202'} alt={'Profile'} sx={{background: '#2026d2', marginLeft: "80px"}} />
                     </Box>
                 </Box>
             </AppBar>
--- src/page-components/editor/Editor.jsx
+++ src/page-components/editor/Editor.jsx
@@ -1,9 +1,47 @@
+import {Grid, Hidden, Box} from "@mui/material";
 
 const Editor = () =>  {
 
     return (
         <>
-            Editor
+            <Grid container spacing={0} height={'auto'}>
+                <Hidden mdDown>
+                    <Grid item xs={0} md={3}>
+                        <Box width={"100%"} p={1}>
+                            <Box width={"100%"} borderRadius={"12px"} sx={{overflow: "hidden"}}>
+                                <Box width={"100%"} p={1.5} bgcolor={"#ededed"} borderBottom={"1px solid rgba(137,137,137,.1)"}>
+                                    Query
+                                </Box>
+                                <Box p={1.5} height={"calc(100vh - 125px)"} bgcolor={"#f9f9f6"}>
+                                    asd
+                                </Box>
+                            </Box>
+                        </Box>
+                    </Grid>
+                </Hidden>
+
+                <Grid item xs={12} md={9}>
+                    <Box width={"100%"} p={1} pl={0.5}>
+                        <Box width={"100%"} borderRadius={"12px"} sx={{overflow: "hidden"}}>
+                            <Box width={"100%"} p={1.5} bgcolor={"#ededed"} borderBottom={"1px solid rgba(137,137,137,.1)"}>
+                                Editor
+                            </Box>
+                            <Box p={1.5} height={"10vh"} bgcolor={"#f9f9f6"}>
+                                SELECT * FROM articleData;
+                            </Box>
+                        </Box>
+
+                        <Box width={"100%"} borderRadius={"12px"} sx={{overflow: "hidden"}} mt={1.5}>
+                            <Box width={"100%"} p={1.5} bgcolor={"#ededed"} borderBottom={"1px solid rgba(137,137,137,.1)"}>
+                                Table
+                            </Box>
+                            <Box p={1.5} height={"calc(90vh - 180px)"} bgcolor={"#f9f9f6"}>
+                                Select query to see the output
+                            </Box>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
         </>
     );
 }
