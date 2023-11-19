deba898c5694f4742f1008c83890f86fdfd3feed
roshanchatei <rkc.mj5@gmail.com>
2023-11-19 05:48:21 +0530

    added where

--- src/utils/getQueryOutput.js
+++ src/utils/getQueryOutput.js
@@ -11,6 +11,30 @@ export const getQueryOutput = (data, query) => {
         });
     }
 
+    if (query.where) {
+        result = result.filter(entry => {
+            return Object.keys(query.where).every(field => {
+                const condition = query.where[field];
+                if (Array.isArray(condition)) {
+                    return condition.includes(entry[field]);
+                } else if (typeof condition === 'object') {
+                    const operator = Object.keys(condition)[0];
+                    const value = condition[operator];
+                    switch (operator) {
+                        case '>':
+                            return entry[field] > value;
+                        case '<':
+                            return entry[field] < value;
+                        default:
+                            return true;
+                    }
+                } else {
+                    return entry[field] === condition;
+                }
+            });
+        });
+    }
+
 
 
     return result;
